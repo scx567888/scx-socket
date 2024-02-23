c90e4bc5f79602e5a04030dae68245640bf8fd7b
scx567888 <scx567888@outlook.com>
2024-02-23 10:48:04 +0800

    dev

---
+++ src/main/java/cool/scx/socket_new/ScxSocketEvent.java
@@ -0,0 +1,43 @@
+package cool.scx.socket_new;
+
+import cool.scx.socket.ScxSocketRequest;
+
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+final class ScxSocketEvent {
+
+    public final byte type;
+    private Consumer<String> event0;
+    private Function<String, String> event1;
+    private BiConsumer<String, ScxSocketRequest> event2;
+
+    public ScxSocketEvent(Consumer<String> eventConsumer) {
+        this.event0 = eventConsumer;
+        this.type = 0;
+    }
+
+    public ScxSocketEvent(Function<String, String> eventFunction) {
+        this.event1 = eventFunction;
+        this.type = 1;
+    }
+
+    public ScxSocketEvent(BiConsumer<String, ScxSocketRequest> eventBiConsumer) {
+        this.event2 = eventBiConsumer;
+        this.type = 2;
+    }
+
+    public Consumer<String> event0() {
+        return event0;
+    }
+
+    public Function<String, String> event1() {
+        return event1;
+    }
+
+    public BiConsumer<String, ScxSocketRequest> event2() {
+        return event2;
+    }
+
+}
