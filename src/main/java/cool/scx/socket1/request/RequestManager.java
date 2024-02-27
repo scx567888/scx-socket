6a7cdeb74de6f2b9815a30b05e62497195fb8ab7
scx567888 <scx567888@outlook.com>
2024-02-27 19:27:20 +0800

    dev

---
+++ src/main/java/cool/scx/socket1/request/RequestManager.java
@@ -0,0 +1,37 @@
+package cool.scx.socket1.request;
+
+import cool.scx.socket1.frame.ScxSocketFrame;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.function.BiConsumer;
+
+public class RequestManager {
+
+    final ConcurrentMap<Long, RequestTask> responseTaskMap;
+
+    public RequestManager() {
+        this.responseTaskMap = new ConcurrentHashMap<>();
+    }
+
+    protected final void setResponseCallback(ScxSocketFrame socketFrame, BiConsumer<String, Throwable> responseCallback, RequestOptions options) {
+        var requestTask = new RequestTask(responseCallback, this, options, socketFrame.seq_id);
+        this.responseTaskMap.put(socketFrame.seq_id, requestTask);
+        requestTask.start();
+    }
+
+    protected void callResponseCallback(ScxSocketFrame socketFrame) {
+        var requestTask = this.responseTaskMap.get(socketFrame.ack_id);
+        if (requestTask != null) {
+            requestTask.success(socketFrame.payload);
+        }
+    }
+
+    protected void callResponseCallbackAsync(ScxSocketFrame socketFrame) {
+        var requestTask = this.responseTaskMap.get(socketFrame.ack_id);
+        if (requestTask != null) {
+            Thread.ofVirtual().start(() -> requestTask.success(socketFrame.payload));
+        }
+    }
+
+}
