1db117a6fb2faf41ce64855e49554fd362d359f0
scx567888 <scx567888@outlook.com>
2024-02-27 19:18:24 +0800

    dev

---
+++ src/main/java/cool/scx/socket1/request/RequestOptions.java
@@ -0,0 +1,21 @@
+package cool.scx.socket1.request;
+
+import cool.scx.socket1.sender.SendOptions;
+
+public class RequestOptions extends SendOptions {
+
+    private int requestTimeout;
+
+    public RequestOptions() {
+        this.requestTimeout = 1000 * 10; // 默认请求超时 10 秒
+    }
+
+    public int getRequestTimeout() {
+        return requestTimeout;
+    }
+
+    public void setRequestTimeout(int requestTimeout) {
+        this.requestTimeout = requestTimeout;
+    }
+
+}
