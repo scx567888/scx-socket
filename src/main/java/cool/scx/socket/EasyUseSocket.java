7dfd940f4674a4e52b818d0b36ead6517b36664f
scx567888 <scx567888@outlook.com>
2024-03-19 15:59:27 +0800

    dev

--- src/main/java/cool/scx/socket/EasyUseSocket.java
+++ src/main/java/cool/scx/socket/EasyUseSocket.java
@@ -1,6 +1,7 @@
 package cool.scx.socket;
 
 import com.fasterxml.jackson.core.type.TypeReference;
+import io.vertx.core.http.WebSocketBase;
 
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
@@ -10,100 +11,96 @@ import static cool.scx.socket.Helper.fromJson;
 import static cool.scx.socket.Helper.toJson;
 
 /**
- * 便于使用的 接口
+ * 便于使用的 Socket
  */
-public interface EasyUseSocket {
+class EasyUseSocket extends ScxSocket {
 
-    SendOptions DEFAULT_SEND_OPTIONS = new SendOptions();
-    RequestOptions DEFAULT_REQUEST_OPTIONS = new RequestOptions();
+    private static final SendOptions DEFAULT_SEND_OPTIONS = new SendOptions();
+    private static final RequestOptions DEFAULT_REQUEST_OPTIONS = new RequestOptions();
 
-    FrameCreator frameCreator();
-
-    RequestManager requestManager();
-
-    void send(ScxSocketFrame socketFrame, SendOptions options);
-
-    void onEvent(String eventName, Consumer<String> onEvent);
-
-    void onEvent(String eventName, Function<String, String> onEvent);
-
-    void onEvent(String eventName, BiConsumer<String, ScxSocketRequest> onEvent);
-
-    default void send(String content) {
-        send(frameCreator().createMessageFrame(content, DEFAULT_SEND_OPTIONS), DEFAULT_SEND_OPTIONS);
+    public EasyUseSocket(WebSocketBase webSocket, String clientID, ScxSocketOptions options, ScxSocketStatus status) {
+        super(webSocket, clientID, options, status);
     }
 
-    default void send(String content, SendOptions options) {
-        send(frameCreator().createMessageFrame(content, options), options);
+    public EasyUseSocket(WebSocketBase webSocket, String clientID, ScxSocketOptions options) {
+        super(webSocket, clientID, options);
     }
 
-    default void sendEvent(String eventName, String data) {
-        send(frameCreator().createEventFrame(eventName, data, DEFAULT_SEND_OPTIONS), DEFAULT_SEND_OPTIONS);
+    public final void send(String content) {
+        send(status.frameCreator.createMessageFrame(content, DEFAULT_SEND_OPTIONS), DEFAULT_SEND_OPTIONS);
     }
 
-    default void sendEvent(String eventName, String data, SendOptions options) {
-        send(frameCreator().createEventFrame(eventName, data, options), options);
+    public final void send(String content, SendOptions options) {
+        send(status.frameCreator.createMessageFrame(content, options), options);
     }
 
-    default void sendEvent(String eventName, String data, BiConsumer<String, Throwable> responseCallback) {
-        var eventFrame = frameCreator().createRequestFrame(eventName, data, DEFAULT_REQUEST_OPTIONS);
-        requestManager().setResponseCallback(eventFrame, responseCallback, DEFAULT_REQUEST_OPTIONS);
+    public final void sendEvent(String eventName, String data) {
+        send(status.frameCreator.createEventFrame(eventName, data, DEFAULT_SEND_OPTIONS), DEFAULT_SEND_OPTIONS);
+    }
+
+    public final void sendEvent(String eventName, String data, SendOptions options) {
+        send(status.frameCreator.createEventFrame(eventName, data, options), options);
+    }
+
+    public final void sendEvent(String eventName, String data, BiConsumer<String, Throwable> responseCallback) {
+        var eventFrame = status.frameCreator.createRequestFrame(eventName, data, DEFAULT_REQUEST_OPTIONS);
+        status.requestManager.setResponseCallback(eventFrame, responseCallback, DEFAULT_REQUEST_OPTIONS);
         send(eventFrame, DEFAULT_REQUEST_OPTIONS);
     }
 
-    default void sendEvent(String eventName, String data, BiConsumer<String, Throwable> responseCallback, RequestOptions options) {
-        var eventFrame = frameCreator().createRequestFrame(eventName, data, options);
-        requestManager().setResponseCallback(eventFrame, responseCallback, options);
+    public final void sendEvent(String eventName, String data, BiConsumer<String, Throwable> responseCallback, RequestOptions options) {
+        var eventFrame = status.frameCreator.createRequestFrame(eventName, data, options);
+        status.requestManager.setResponseCallback(eventFrame, responseCallback, options);
         send(eventFrame, options);
     }
 
-    default void send(Object data) {
+    public final void send(Object data) {
         this.send(toJson(data));
     }
 
-    default void send(Object data, SendOptions options) {
+    public final void send(Object data, SendOptions options) {
         this.send(toJson(data), options);
     }
 
-    default void sendEvent(String eventName, Object data) {
+    public final void sendEvent(String eventName, Object data) {
         this.sendEvent(eventName, toJson(data));
     }
 
-    default void sendEvent(String eventName, Object data, SendOptions options) {
+    public final void sendEvent(String eventName, Object data, SendOptions options) {
         this.sendEvent(eventName, toJson(data), options);
     }
 
-    default void sendEvent(String eventName, Object data, BiConsumer<String, Throwable> responseCallback) {
+    public final void sendEvent(String eventName, Object data, BiConsumer<String, Throwable> responseCallback) {
         this.sendEvent(eventName, toJson(data), responseCallback);
     }
 
-    default void sendEvent(String eventName, Object data, BiConsumer<String, Throwable> responseCallback, RequestOptions options) {
+    public final void sendEvent(String eventName, Object data, BiConsumer<String, Throwable> responseCallback, RequestOptions options) {
         this.sendEvent(eventName, toJson(data), responseCallback, options);
     }
 
-    default <T> void sendEvent(String eventName, Object data, BiConsumer<T, Throwable> responseCallback, Class<T> tClass) {
+    public final <T> void sendEvent(String eventName, Object data, BiConsumer<T, Throwable> responseCallback, Class<T> tClass) {
         this.sendEvent(eventName, toJson(data), (s, e) -> responseCallback.accept(fromJson(s, tClass), e));
     }
 
-    default <T> void sendEvent(String eventName, Object data, BiConsumer<T, Throwable> responseCallback, RequestOptions options, Class<T> tClass) {
+    public final <T> void sendEvent(String eventName, Object data, BiConsumer<T, Throwable> responseCallback, RequestOptions options, Class<T> tClass) {
         this.sendEvent(eventName, toJson(data), (s, e) -> responseCallback.accept(fromJson(s, tClass), e), options);
     }
 
-    default <T> void sendEvent(String eventName, Object data, BiConsumer<T, Throwable> responseCallback, TypeReference<T> tClass) {
+    public final <T> void sendEvent(String eventName, Object data, BiConsumer<T, Throwable> responseCallback, TypeReference<T> tClass) {
         this.sendEvent(eventName, toJson(data), (s, e) -> responseCallback.accept(fromJson(s, tClass), e));
     }
 
-    default <T> void sendEvent(String eventName, Object data, BiConsumer<T, Throwable> responseCallback, RequestOptions options, TypeReference<T> tClass) {
+    public final <T> void sendEvent(String eventName, Object data, BiConsumer<T, Throwable> responseCallback, RequestOptions options, TypeReference<T> tClass) {
         this.sendEvent(eventName, toJson(data), (s, e) -> responseCallback.accept(fromJson(s, tClass), e), options);
     }
 
-    default <T> void onEvent(String eventName, Consumer<T> onEvent, Class<T> tClass) {
+    public final <T> void onEvent(String eventName, Consumer<T> onEvent, Class<T> tClass) {
         this.onEvent(eventName, s -> {
             onEvent.accept(fromJson(s, tClass));
         });
     }
 
-    default <T> void onEvent(String eventName, Function<T, ?> onEvent, Class<T> tClass) {
+    public final <T> void onEvent(String eventName, Function<T, ?> onEvent, Class<T> tClass) {
         this.onEvent(eventName, s -> {
             var data = onEvent.apply(fromJson(s, tClass));
             if (data instanceof String str) {
@@ -114,13 +111,13 @@ public interface EasyUseSocket {
         });
     }
 
-    default <T> void onEvent(String eventName, Consumer<T> onEvent, TypeReference<T> tClass) {
+    public final <T> void onEvent(String eventName, Consumer<T> onEvent, TypeReference<T> tClass) {
         this.onEvent(eventName, s -> {
             onEvent.accept(fromJson(s, tClass));
         });
     }
 
-    default <T> void onEvent(String eventName, Function<T, ?> onEvent, TypeReference<T> tClass) {
+    public final <T> void onEvent(String eventName, Function<T, ?> onEvent, TypeReference<T> tClass) {
         this.onEvent(eventName, s -> {
             var data = onEvent.apply(fromJson(s, tClass));
             if (data instanceof String str) {
@@ -131,19 +128,19 @@ public interface EasyUseSocket {
         });
     }
 
-    default <T> void onEvent(String eventName, BiConsumer<T, TypeRequest> onEvent, Class<T> tClass) {
+    public final <T> void onEvent(String eventName, BiConsumer<T, TypeRequest> onEvent, Class<T> tClass) {
         this.onEvent(eventName, (s, r) -> {
             onEvent.accept(fromJson(s, tClass), new TypeRequest(r));
         });
     }
 
-    default <T> void onEvent(String eventName, BiConsumer<T, TypeRequest> onEvent, TypeReference<T> tClass) {
+    public final <T> void onEvent(String eventName, BiConsumer<T, TypeRequest> onEvent, TypeReference<T> tClass) {
         this.onEvent(eventName, (s, r) -> {
             onEvent.accept(fromJson(s, tClass), new TypeRequest(r));
         });
     }
 
-    class TypeRequest {
+    public static class TypeRequest {
 
         private final ScxSocketRequest request;
 
