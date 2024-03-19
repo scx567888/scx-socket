eb0aaa3282bda7cedf7a0aa363e0055c9c270363
scx567888 <scx567888@outlook.com>
2024-03-19 15:45:10 +0800

    dev

--- src/main/java/cool/scx/socket/ScxSocketServer.java
+++ src/main/java/cool/scx/socket/ScxSocketServer.java
@@ -62,23 +62,4 @@ public final class ScxSocketServer {
         return serverSockets.values();
     }
 
-    public static final class ScxSocketServerOptions extends PingPongOptions {
-
-        private int statusKeepTime;
-
-        public ScxSocketServerOptions() {
-            this.statusKeepTime = 1000 * 60 * 30; // 默认 30 分钟
-        }
-
-        public int getStatusKeepTime() {
-            return statusKeepTime;
-        }
-
-        public ScxSocketServerOptions setStatusKeepTime(int statusKeepTime) {
-            this.statusKeepTime = statusKeepTime;
-            return this;
-        }
-
-    }
-
 }
