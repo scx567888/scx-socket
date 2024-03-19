815167a7f65a4ddc6768016d71d6f4e49380f8c0
scx567888 <scx567888@outlook.com>
2024-03-19 14:03:54 +0800

    dev

--- src/main/java/cool/scx/socket/ScxSocketFrame.java
+++ src/main/java/cool/scx/socket/ScxSocketFrame.java
@@ -5,23 +5,68 @@ package cool.scx.socket;
  */
 public class ScxSocketFrame {
 
+    /**
+     * 序列 ID
+     */
     public long seq_id;
+
+    /**
+     * 类型  参见 {@link Type}
+     */
     public byte type;
+
+    /**
+     * 时间戳
+     */
     public long now;
+
+    /**
+     * 事件名称
+     */
     public String event_name;
+
+    /**
+     * 有效载荷
+     */
     public String payload;
+
+    /**
+     * 应答 ID
+     */
     public long ack_id;
+
+    /**
+     * 是否需要 应答
+     */
     public boolean need_ack;
+
+    /**
+     * 是否需要 响应
+     */
     public boolean need_response;
 
+    /**
+     * 从 JSON 反序列化
+     *
+     * @param json json
+     * @return a
+     */
     public static ScxSocketFrame fromJson(String json) {
         return Helper.fromJson(json, ScxSocketFrame.class);
     }
 
+    /**
+     * 序列化到 JSON
+     *
+     * @return json
+     */
     public String toJson() {
         return Helper.toJson(this);
     }
 
+    /**
+     * 类型定义
+     */
     public static class Type {
 
         /**
