45ba826c8dde05eb91ab5f691dcf10d5eb3b31ca
scx567888 <scx567888@outlook.com>
2024-03-19 16:20:55 +0800

    dev

--- src/main/java/cool/scx/socket/RequestManager.java
+++ src/main/java/cool/scx/socket/RequestManager.java
@@ -4,7 +4,7 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 import java.util.function.BiConsumer;
 
-public class RequestManager {
+final class RequestManager {
 
     final ConcurrentMap<Long, RequestTask> responseTaskMap;
 
@@ -12,23 +12,16 @@ public class RequestManager {
         this.responseTaskMap = new ConcurrentHashMap<>();
     }
 
-    public final void setResponseCallback(ScxSocketFrame socketFrame, BiConsumer<String, Throwable> responseCallback, RequestOptions options) {
+    public void setResponseCallback(ScxSocketFrame socketFrame, BiConsumer<String, Throwable> responseCallback, RequestOptions options) {
         var requestTask = new RequestTask(responseCallback, this, options, socketFrame.seq_id);
         this.responseTaskMap.put(socketFrame.seq_id, requestTask);
         requestTask.start();
     }
 
-    protected void success(ScxSocketFrame socketFrame) {
+    public void success(ScxSocketFrame socketFrame) {
         var requestTask = this.responseTaskMap.get(socketFrame.ack_id);
         if (requestTask != null) {
             requestTask.success(socketFrame.payload);
-        }
-    }
-
-    public void successAsync(ScxSocketFrame socketFrame) {
-        var requestTask = this.responseTaskMap.get(socketFrame.ack_id);
-        if (requestTask != null) {
-            Thread.ofVirtual().start(() -> requestTask.success(socketFrame.payload));
         }
     }
 
