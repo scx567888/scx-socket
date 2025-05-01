dd5f18d5c2e00d3b86733d1a8d7356f51e3a6850
scx567888 <scx567888@outlook.com>
2025-05-01 19:16:59 +0800

    dev

--- src/main/java/cool/scx/socket/PingPongManager.java
+++ src/main/java/cool/scx/socket/PingPongManager.java
@@ -2,7 +2,7 @@ package cool.scx.socket;
 
 
 import cool.scx.websocket.ScxWebSocket;
-import cool.scx.websocket.handler.ScxEventWebSocket;
+import cool.scx.websocket.event.ScxEventWebSocket;
 
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
